82c9447752a68b2a06d796ba87f39587722ffbc2
Matheus Borges <matheus.borges@oobj.com.br>
2022-08-29 20:32:31 -0300

    ajuste dos pacotes e inicio da definição de entidades

---
+++ biblioteca/src/main/java/com/poo/biblioteca/entidades/Autor.java
@@ -0,0 +1,49 @@
+package com.poo.biblioteca.entidades;
+
+import java.util.Date;
+
+public class Autor {
+    private String nome;
+    private String nacionalidade;
+    private String formacao;
+    private Date dataNascimento;
+
+    public Autor(String nome, String nacionalidade, String formacao, Date dataNascimento) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.formacao = formacao;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+    
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+    
+    public String getFormacao() {
+        return formacao;
+    }
+    
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+    
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+    
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
