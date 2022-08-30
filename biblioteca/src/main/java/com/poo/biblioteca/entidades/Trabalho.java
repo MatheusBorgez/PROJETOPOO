5ca8980b891ecc287bf1a9f906f8cac20e08171d
Matheus Borges <matheus.borges@oobj.com.br>
2022-08-29 21:37:07 -0300

    definição de entidades

--- biblioteca/src/main/java/com/poo/biblioteca/entidades/Trabalho.java
+++ biblioteca/src/main/java/com/poo/biblioteca/entidades/Trabalho.java
@@ -5,36 +5,41 @@ import java.util.ArrayList;
 
 import com.poo.biblioteca.enumerator.TipoTrabalho;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "trabalho")
+@MappedSuperclass
 public abstract class Trabalho implements IDocumento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private TipoTrabalho tipoTrabalho;
+    @Column(name = "tipo_trabalho")
+    private int tipoTrabalho;
+
+    @Column(name = "titulo")
     private String titulo;
+
     private ArrayList<Autor> autores = new ArrayList<>();
 
     public Trabalho(String titulo, ArrayList<Autor> autores, TipoTrabalho tipoTrabalho) {
         this.titulo = titulo;
         this.autores = autores;
-        this.tipoTrabalho = tipoTrabalho;
+        this.tipoTrabalho = tipoTrabalho.getValue();
     }
 
-    public TipoTrabalho getTipoTrabalho() {
+    public int getTipoTrabalho() {
         return tipoTrabalho;
     }
 
     public void setTipoTrabalho(TipoTrabalho tipoTrabalho) {
-        this.tipoTrabalho = tipoTrabalho;
+        this.tipoTrabalho = tipoTrabalho.getValue();
     }
 
     @Override
