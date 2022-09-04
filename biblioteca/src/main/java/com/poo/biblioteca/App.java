471f13d25df68ccab1e7e6894ed7f83a796fff1c
eadaianne <eadaianne@gmail.com>
2022-09-04 01:39:39 -0300

    Cria interfaces

--- biblioteca/src/main/java/com/poo/biblioteca/App.java
+++ biblioteca/src/main/java/com/poo/biblioteca/App.java
@@ -6,17 +6,12 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-/**
- * Hello world!
- *
- */
 public class App {
 
     private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("biblioteca");
     private static EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         Autor cliente = entityManager.find(Autor.class, 1);        
     }
 }
